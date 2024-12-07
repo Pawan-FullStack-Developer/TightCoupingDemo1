9e45f87660d12bed92dd85890d03cb4af2bd2096
WINDOWS1O <WINDOWS1O@DESKTOP-HNTOU4H>
2024-12-07 18:02:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TightCoupingDemo1
+{
+    
+    
+        //Tight Coupling
+
+        //creating Different types of Engine classes
+
+        public class PetrolEngine
+        {
+            public void Start()
+            {
+                Console.WriteLine( "Petrol engine started...");
+            }
+        }
+        public class DieselEngine
+        {
+            public void Start()
+            {
+                Console.WriteLine("Diesel engine started...");
+            }
+        }
+        public class ElectricEngine
+        {
+            public void Start()
+            {
+                Console.WriteLine("Electric engine started...");
+            }
+        }
+        //creating Car class
+        public class Car
+        {
+            // PetrolEngine engine; //object is declared
+            //DieselEngine engine;
+            ElectricEngine engine;
+            public Car()
+            {
+                engine = new ElectricEngine(); //creating engine class object
+                                               //engine=new DieselEngine();
+                                               //engine=new PetrolEngine();
+            }
+            public void Start()
+            {
+                engine.Start();
+                Console.WriteLine("Car started....");
+            }
+        }
+        internal class Program
+        { 
+
+
+            static void Main(string[] args)
+            {
+                //creating Car object
+                Car myCar = new Car();
+                myCar.Start(); //starting car
+                Console.ReadKey();
+
+            }
+
+        
+        }
+}
